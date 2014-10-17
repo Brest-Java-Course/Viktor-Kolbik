c704d1bc144e561c96c18bac5f46299811bad9f2
GomerSimpson <bartsimpson53@rambler.ru>
2014-10-17 16:30:49 +0300

    app-core

--- app-core/src/main/java/com/epam/brest/courses/domain/User.java
+++ app-core/src/main/java/com/epam/brest/courses/domain/User.java
@@ -13,4 +13,20 @@ public class User{
                 this.userId = userId;
         }
 
+        public String getLogin(){
+                return login;
+        }
+
+        public void setLogin(String login){
+                this.login = login;
+        }
+
+        public String getUserName(){
+                return userName;
+        }
+
+        public void setUserName(String userName){
+                this.userName = userName;
+        }
+
 }
