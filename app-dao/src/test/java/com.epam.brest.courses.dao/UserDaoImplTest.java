71c8678e79674ef4ae0f4502aa6f8863eb33bbf8
GomerSimpson <bartsimpson53@rambler.ru>
2014-10-22 19:20:35 +0300

    + spring and tests

---
+++ app-dao/src/test/java/com.epam.brest.courses.dao/UserDaoImplTest.java
@@ -0,0 +1,12 @@
+package com.epam.brest.courses.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by simpson on 22.10.14.
+ */
+public class UserDaoImplTest {
+
+    @Autowired
+    UserDao userDao;
+}
