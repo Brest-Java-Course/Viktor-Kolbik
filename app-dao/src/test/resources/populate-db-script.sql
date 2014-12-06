c1789a93524d34d1392e5be7dcda4ce3e38a72ac
GomerSimpson <bartsimpson53@rambler.ru>
2014-12-06 13:03:26 +0300

    fixed creation db

--- app-dao/src/test/resources/populate-db-script.sql
+++ app-dao/src/test/resources/populate-db-script.sql
@@ -1,15 +1,8 @@
 INSERT INTO GALAXY (name, distance, discoverDate) VALUES ('G0', 0, '2014-05-01');
 INSERT INTO GALAXY (name, distance, discoverDate) VALUES ('G1', 1, '2014-05-01');
 INSERT INTO GALAXY (name, distance, discoverDate) VALUES ('G2', 2, '2014-05-02');
-INSERT INTO GALAXY (name, distance, discoverDate) VALUES ('G3', 3, '2014-05-03');
-INSERT INTO GALAXY (name, distance, discoverDate) VALUES ('G4', 4, '2014-05-04');
-INSERT INTO GALAXY (name, distance, discoverDate) VALUES ('G5', 5, '2014-05-05');
 
 INSERT INTO STAR (name, age, mass, discoverDate, galaxyId) VALUES ('S1', 1000, 1, '2014-01-01', 0);
 INSERT INTO STAR (name, age, mass, discoverDate, galaxyId) VALUES ('S2', 2000, 3, '2014-01-02', 0);
 INSERT INTO STAR (name, age, mass, discoverDate, galaxyId) VALUES ('S3', 3000, 5, '2014-01-03', 0);
 INSERT INTO STAR (name, age, mass, discoverDate, galaxyId) VALUES ('S4', 4000, 7, '2014-01-04', 1);
-INSERT INTO STAR (name, age, mass, discoverDate, galaxyId) VALUES ('S5', 5000, 9, '2014-01-05', 1);
-INSERT INTO STAR (name, age, mass, discoverDate, galaxyId) VALUES ('S6', 6000, 11, '2014-01-06', 2);
-INSERT INTO STAR (name, age, mass, discoverDate, galaxyId) VALUES ('S7', 7000, 13, '2014-01-07', 2);
-INSERT INTO STAR (name, age, mass, discoverDate, galaxyId) VALUES ('S8', 7000, 6, '2014-01-08', 2);
